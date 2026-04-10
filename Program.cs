ec6a91dfb82f4202ac423d87faaa51e3354bf444
mythi05 <thi21112005@gmail.com>
2026-04-10 08:43:34 +0700

    enable swagger production

--- Program.cs
+++ Program.cs
@@ -1,21 +1,29 @@
-﻿
-using aspp.Data;
+﻿using aspp.Data;
 using Microsoft.EntityFrameworkCore;
-var builder = WebApplication.CreateBuilder(args);
-// Đăng ký SQL Server
-builder.Services.AddDbContext<AppDbContext>(options =>
 
-options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+var builder = WebApplication.CreateBuilder(args);
+
+// DB
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Services
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
-// Cấu hình Middleware
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+// 🔥 LUÔN bật Swagger (không dùng if)
+app.UseSwagger();
+app.UseSwaggerUI();
+
+// Middleware
 app.UseHttpsRedirection();
+
 app.MapControllers();
+
+// optional: tránh 404 trang chủ
+app.MapGet("/", () => "API is running...");
+
 app.Run();
